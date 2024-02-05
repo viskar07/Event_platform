9f6d298700d59f765cdb617b22f58c677e827e2c
Viskar࿐ <viskar.code@gmail.com>
2024-02-05 16:59:08 +0530

    7th commit

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,17 +1,23 @@
+import CategoryFilter from '@/components/shared/CategoryFilter';
 import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
-import { getAllEvents } from '@/lib/actions/event.action'
+import { getAllEvents } from '@/lib/actions/event.action';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
-const Home = async () => {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
   const events = await getAllEvents({
-    query: '',
-    category:'',
-    page: 1,
-    limit: 6,
-  });
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
 
   return (
     <>
@@ -37,27 +43,24 @@ const Home = async () => {
         </div>
       </section> 
 
-      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-          <h2 className='h2-bold'>Trust by <br /> Thousands of Events</h2>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
-          <div className='flex w-full flex-col gap-5 md:flex-row'>
-            Search
-            CategoryFilter
-          </div>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
 
-          <Collection
-            data={events?.data}
-            emptyTitle='No Events Found'
-            emptyStateSubtext="Come back later"
-            collectionType='All_Events'
-            limit={6}
-            page={1}
-            totalPages={2}
-          />
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
       </section>
-
     </>
   )
 }
-
-export default Home
