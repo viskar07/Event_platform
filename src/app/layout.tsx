5488ff6d27729c04a43a19c6dd4813c067dbedf1
viskar07 <viskar.code@gmail.com>
2024-10-18 05:16:24 +0530

    Updated functionality X and fixed bug Y

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ const poppins = Poppins({
   });
 
 export const metadata: Metadata = {
-  title: "Evently",
-  description: "Evently is platform for event management ",
+  title: "Clubsphere",
+  description: "Effortlessly Create Your Club or Organization ",
   icons: {
     icon:'/assets/images/logo.svg'
   }
